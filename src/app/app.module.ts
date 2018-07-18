85cd207ecf455dcd66180ccc7ab70f1738d4d4e8
FernandoCostaB <fbc@icomp.ufam.edu.br>
2018-07-18 16:01:56 -0400

    insercao do pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule} from '@angular/http';
 import { RouterModule} from '@angular/router'
 
@@ -14,6 +14,10 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { AondeFicaComponent } from './oferta/aonde-fica/aonde-fica.component';
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt";
+registerLocaleData(localePt);
+
 
 @NgModule({
   declarations: [
@@ -32,7 +36,7 @@ import { AondeFicaComponent } from './oferta/aonde-fica/aonde-fica.component';
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [ {provide: LOCALE_ID, useValue: 'pt-Br'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
