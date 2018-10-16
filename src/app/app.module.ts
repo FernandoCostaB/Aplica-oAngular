fa7939173e927266b6ec87e88ccf83a9b63ce291
FernandoCostaB <fbc@icomp.ufam.edu.br>
2018-10-16 16:24:30 -0400

    ng submit para o formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule} from '@angular/http';
-import { RouterModule} from '@angular/router'
-
-import { ROUTES } from './app.routers'
+import { RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ROUTES } from './app.routers';
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
@@ -42,6 +42,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ {provide: LOCALE_ID, useValue: 'pt-Br'}],
